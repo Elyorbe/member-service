f8fe68feeaeb388e6f2b7b51db2586c41ac5a0a6
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2022-11-07 16:15:43 +0900

    Initial commit

---
+++ src/main/java/me/elyor/memberservice/MemberService.java
@@ -0,0 +1,13 @@
+package me.elyor.memberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberService.class, args);
+    }
+
+}
---
+++ src/test/java/me/elyor/memberservice/MemberServiceTests.java
@@ -0,0 +1,13 @@
+package me.elyor.memberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberServiceTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
