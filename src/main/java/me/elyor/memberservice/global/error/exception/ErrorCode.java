5f4d7bbe33cd59db1e2ec326b763c9b941daa0be
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2022-11-12 20:54:17 +0900

    Add: Duplicate username error code

--- src/main/java/me/elyor/memberservice/global/error/exception/ErrorCode.java
+++ src/main/java/me/elyor/memberservice/global/error/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     METHOD_NOT_ALLOWED("Method Not Allowed", HttpStatus.METHOD_NOT_ALLOWED.value()),
     INTERNAL_SERVER_ERROR("Internal server error", HttpStatus.INTERNAL_SERVER_ERROR.value()),
 
+    DUPLICATE_USERNAME("동일한 사용자 아이디가 이미 존재합니다", HttpStatus.CONFLICT.value()),
     DUPLICATE_RESOURCE("리소스가 이미 존재합니다. 다른 값을 사용해 보세요", HttpStatus.CONFLICT.value()),
     INVALID_INPUT_VALUE("입력 값이 잘못되었습니다", HttpStatus.BAD_REQUEST.value()),
     MISSING_REQUEST_PARAM("필수 요청 파라미터가 없습니다", HttpStatus.BAD_REQUEST.value()),
