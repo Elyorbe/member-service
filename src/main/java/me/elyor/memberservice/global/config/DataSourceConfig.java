256022e89562987d135bfc8aafb7cb5c6743606e
Elyorbek Ibrokhimov <eliyoribek@gmail.com>
2022-11-12 20:53:14 +0900

    Add: Transaction support and mybatis config

---
+++ src/main/java/me/elyor/memberservice/global/config/DataSourceConfig.java
@@ -0,0 +1,23 @@
+package me.elyor.memberservice.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfig {
+
+    private DataSource dataSource;
+
+    public DataSourceConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+}
